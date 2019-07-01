60186756bbc96efb3138cc2889f35ab9e47267e2
ADMIN <harinid33@gmail.com>
2019-07-01 06:44:36 -0700

    removed comment in master

--- Flask1_ConnectToForm.py
+++ Flask1_ConnectToForm.py
@@ -5,8 +5,7 @@
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
-# this is the test code
-#test line 2
+
 @app.route('/')
 def my_form():
     return render_template('my-form.html')
